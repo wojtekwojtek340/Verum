7678e208d3cc6eb4f4d7b47cd6cc4e5f8fa77c22
wojtekwojtek340 <51856023+wojtekwojtek340@users.noreply.github.com>
2021-03-05 20:37:30 +0100

    Moving window done

--- VerumPostManager/Verum.WPF/Behaviors/MovingWindow.cs
+++ VerumPostManager/Verum.WPF/Behaviors/MovingWindow.cs
@@ -17,44 +17,12 @@ namespace Verum.WPF.Behaviors
             if(window != null)
             {
                 window.MouseDown += Window_MouseDown;
-                window.MouseMove += Window_MouseMove;
-                window.MouseUp += Window_MouseUp;
             }
         }
-
-        private bool isMoveing = false;
-        Point cursorPossition;
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Window window = (Window)sender;
-
-            if(isMoveing && e.LeftButton == MouseButtonState.Pressed)
-            {
-                isMoveing = true;
-                cursorPossition = e.GetPosition(window);
-            }
-        }
-        private void Window_MouseMove(object sender, MouseEventArgs e)
-        {
-            Window window = (Window)sender;
-
-            if (isMoveing)
-            {
-                Point nowCursorPossition = e.GetPosition(window);
-                Vector displacement = nowCursorPossition - cursorPossition;
-                window.Left += displacement.X;
-                window.Top += displacement.Y;
-            }
-        }
-
-        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            Window window = (Window)sender;
-
-            if (isMoveing && e.LeftButton == MouseButtonState.Released)
-            {
-                isMoveing = false;
-            }
+            window.DragMove();
         }
     }
 }
